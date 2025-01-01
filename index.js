f13ca4f9662276027890bf0550c909d2e8d4f838
Ald28 <aldopichihua123@gmail.com>
2024-12-31 21:51:34 -0500

    nuevos estilos

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const profileImg = document.querySelector('.perfil-img');
+    profileImg.addEventListener('click', () => {
+        alert('¡Hola! Gracias por visitar mi portafolio.');
+    });
+});
